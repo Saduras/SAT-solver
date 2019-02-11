fd771836edd28c82814f9db2eb657f2d7fcd2973
David Speck <david@saduras.de>
2019-02-11 15:56:34 +0100

    Add main and function to preprocess concatenation

--- preprocess_sudoku.py
+++ preprocess_sudoku.py
@@ -1,21 +1,30 @@
 import sys;
 
-if(len(sys.argv) != 3):
-    print("Missing arguments! Expecting 'sudoku-filename' 'output-path'")
-    exit()
+def append_rules(filename, rule_path, output):
+    """
+    Concatenate DIMACS sudoku with DIMACS sudoku rules to signale file
+    input:
+        filename - path to sudoku in DIMACS format
+        rule_path - path to sudoku rule set in DIMACS format
+        output - path to store result
+    """
+    with open(rule_path, 'r') as file:
+        rules = file.read()
 
-rule_path = './data/sudoku-rules.txt'
-filename = sys.argv[1]
-output = sys.argv[2]
+    with open(filename, 'r') as file:
+        sudoku = file.read()
 
-with open(rule_path, 'r') as file:
-    rules = file.read()
+    with open(output,'w') as file:
+        file.write(rules + sudoku)
+    print('Created combined file at ' + output)
 
-with open(filename, 'r') as file:
-    sudoku = file.read()
+if __name__ == "__main__":
+    if(len(sys.argv) != 3):
+        print("Missing arguments! Expecting 'sudoku-filename' 'output-path'")
+        exit()
 
-with open(output,'w') as file:
-    file.write(rules + sudoku)
+    rule_path = './data/sudoku-rules.txt'
+    filename = sys.argv[1]
+    output = sys.argv[2]
 
-print('Created combined file at ' + output)
-
+    append_rules(filename, output)
