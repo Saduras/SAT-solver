1e051e95cfe992dc81f7a57447baa8fe05abef62
Victor <victorzuanazzi@gmail.com>
2019-02-11 16:15:08 +0100

    converted: "damnhard"

--- save_all_sudoku.py
+++ save_all_sudoku.py
@@ -44,6 +44,6 @@ def load_all_sudoku(path, name):
 
 
 if __name__ == "__main__":
-    path = ".\\data\\1000 sudokus.txt"
-    name = "1000 sudokus"
+    path = ".\\data\\damnhard.sdk.txt"
+    name = "damnhard"
     sdk = load_all_sudoku(path, name)
