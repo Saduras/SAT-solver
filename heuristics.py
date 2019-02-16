a319e13d1bba91c8b1bd13c8666cb70c72e64ac2
Victor <victorzuanazzi@gmail.com>
2019-02-16 17:43:08 +0100

    popular_literal inefficiently implemented

---
+++ heuristics.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 17:04:17 2019
+
+@author: Victor Zuanazzi
+"""
+
+def popular_literal(cnf):
+    """returns the literal that appears in most clauses
+    """
+    
+    #ineficient implementation
+    literal_count = {}
+    for clause in cnf:
+        for literal in clause:
+            literal_count[literal] = literal_count.get(literal, 0) + 1
+            
+    return max(literal_count, key=lambda key: literal_count[key])
+
+
+def clause_killer(cnf):
+    """returns the literal that solves one clause (and possibly the assigment)
+    """
+    pass
