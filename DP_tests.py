2375dde1570571ad5330b412e23d467880078bbe
David Speck <david@saduras.de>
2019-02-19 13:02:06 +0100

    Fix BOHM heuristic

--- DP_tests.py
+++ DP_tests.py
@@ -200,6 +200,24 @@ class DPTests(unittest.TestCase):
         
         literal = DLIS(cnf)
         self.assertEqual(literal, (-123, True))
+    
+    def test_BOHM_trivial(self):
+        cnf = [{-123:True}]
+        
+        literal = BOHM(cnf)
+        self.assertEqual(literal, (-123, True))
+
+    def test_BOHM_multiClauseTie(self):
+        cnf = [{-123:True, 234:True}, {123:True, -234:True, 456:True}]
+        
+        literal = BOHM(cnf)
+        self.assertNotEqual(literal, (456, True))
+
+    def test_BOHM_shortClausePrio(self):
+        cnf = [{-123:True, 234:True, 345:True}, {345:True}, {234:True, 567:True}]
+        
+        literal = BOHM(cnf)
+        self.assertEqual(literal, (345, True))
         
     def test_nextLiteral(self):
         cnf = [{-123:True, -312:True, 423:True, 123:True},
--- heuristics.py
+++ heuristics.py
@@ -7,6 +7,7 @@ Created on Sat Feb 16 17:04:17 2019
 
 import numpy as np
 import random
+from collections import Counter
 
 def nextLiteral(cnf):
     """returns the first literal in the first clause.
@@ -24,7 +25,7 @@ def DLIS(cnf):
     """returns the literal that appears in most clauses
     """
     
-    #ineficient implementation
+    #inefficient implementation
     literal_count = {}
     for clause in cnf:
         for literal in clause:
@@ -46,53 +47,41 @@ def BOHM(cnf):
     #store the score of each literal
     literal_score = {}
     
-    #dictionary that stores the clauses indexed in their length
-    len_clauses = {}
-    
-    #hyperparameters suggested to be set to those values
+    #hyper-parameters suggested to be set to those values
     alpha = 1
     beta = 2
     
-    #makes the dictionary that stores the clauses indexed in their length
-    for clause in cnf:
-        if len_clauses.get(len(clause), True):
-            len_clauses[len(clause)] = []
-        len_clauses[len(clause)].append(clause)                
-    
-    #Loop over all literals
-    for clause in cnf:
-        for literal in clause:
-            
-            #negative literals are evaluated together with positive literals
-            if literal < 0: 
-                continue
+    #counter of literals per clause length
+    len_lit_count = {}
+    literals = set()
 
-            vector = []
-            
-            #Loop over all length of clauses:
-            for lc in len_clauses:
-                pos_count = 0
-                neg_count = 0
-                
-                #for every literal in the every clause, check if it is the 
-                #same as the literal of interest
-                for c in len_clauses[lc]:
-                    for lit in c:
-                        if lit == literal:
-                            pos_count += 1
-                        if lit == -literal:
-                            neg_count += 1
-                            
-                vector.append(alpha*max(pos_count, neg_count) + 
-                         beta*min(pos_count, neg_count))
-            
-            #unsure of implementation
-            literal_score[literal] = np.linalg.norm(np.array(vector))
+    for clause in cnf:
+        l = len(clause)
+        if(not l in len_lit_count):
+            len_lit_count[l] = Counter()
+        for literal in clause:
+            len_lit_count[l][literal] += 1
+            literals.add(literal)
+
     
+    literal_score = []
+
+    #Loop over all literals
+    for literal in literals:
+        vector = []
+        
+        #Loop over all length of clauses:
+        for l in len_lit_count.keys():
+            score = alpha*max(len_lit_count[l][literal], len_lit_count[l][-literal]) 
+            score += beta*min(len_lit_count[l][literal], len_lit_count[l][-literal])
+            vector.append(score)
+        
+        literal_score.append( (literal, vector) )
+
     #returns the literal that is not dominated by any other. 
-    #unsure of implementation.
-    bohms_favorite =  max(literal_score, key=lambda key: literal_score[key])
-    value = True 
+    literal_score.sort(key= lambda item: item[1], reverse=True)
+    bohms_favorite = literal_score[0][0]
+    value = True
     
     return bohms_favorite, value
 
