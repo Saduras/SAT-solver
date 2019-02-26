fbbd80459d5a4e98ac3f1523fd12bae8edf715f8
Victor <victorzuanazzi@gmail.com>
2019-02-26 17:41:17 +0100

    fixed path dependencies

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import pandas as pd
 
-def saveSS(cnf, assignment):
+
+def saveSS(cnf, assignment, path = '../data/SSplits.csv' ):
     """Saves the sudoku state in the file SSplits.csv
     """
     
@@ -20,13 +21,12 @@ def saveSS(cnf, assignment):
                       columns = [x for x in range(81)], 
                       dtype = "int_")
     
-    path = './data/SSplits.csv'
     try:
         df.to_csv(path_or_buf = path, mode = 'a', header = False)
     except:
         pass
         
-def saveLabel(assignment, n_splits):
+def saveLabel(assignment, n_splits, path = '../data/SplitsLabel.csv'):
     """save the finished sudoku in a .csv
     """
     
@@ -39,7 +39,6 @@ def saveLabel(assignment, n_splits):
 
     df = pd.DataFrame(label, columns = [x for x in range(len(sudoku_solution))])
     
-    path = './data/SplitsLabel.csv'
     try:
         df.to_csv(path_or_buf = path, 
                   mode = 'a',
@@ -55,6 +54,4 @@ def DIMACS_to_sudoku(assignment):
         matrix[v[0] - 1][v[1] - 1] = v[2]
     return matrix
 
-def assignmentToDIMACSFile(assignment, filename):
-    with open(filename,'w') as file:
-        file.write('\n'.join([str(v) for v in assignment]))
+
