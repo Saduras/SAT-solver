415930dac84f0e645b6d285d87f9e9d4bc31d3db
David Speck <david@saduras.de>
2019-03-01 16:10:58 +0100

    Test: Remove value from heuristics test to fix them

--- src/heuristics_tests.py
+++ src/heuristics_tests.py
@@ -6,45 +6,45 @@ class DPTests(unittest.TestCase):
         cnf = [{-123:True}]
         
         literal = DLIS(cnf)
-        self.assertEqual(literal, (-123, True))
+        self.assertEqual(literal, -123)
     
     def test_DLIS_tie(self):
         cnf = [{-123:True, 234:True}]
         
         literal = DLIS(cnf)
-        self.assertEqual(literal, (-123, True))
+        self.assertEqual(literal, -123)
 
     def test_DLIS_twoClause(self):
         cnf = [{-123:True, -312:True, 423:True, 123:True},
                {-123:True}]
         
         literal = DLIS(cnf)
-        self.assertEqual(literal, (-312, True))
+        self.assertEqual(literal, -312)
     
     def test_BOHM_trivial(self):
         cnf = [{-123:True}]
         
         literal = BOHM(cnf)
-        self.assertEqual(literal, (-123, True))
+        self.assertEqual(literal, -123)
 
     def test_BOHM_multiClauseTie(self):
         cnf = [{-123:True, 234:True}, {123:True, -234:True, 456:True}]
         
         literal = BOHM(cnf)
-        self.assertNotEqual(literal, (456, True))
+        self.assertNotEqual(literal, 456)
 
     def test_BOHM_shortClausePrio(self):
         cnf = [{-123:True, 234:True, 345:True}, {345:True}, {234:True, 567:True}]
         
         literal = BOHM(cnf)
-        self.assertEqual(literal, (345, True))
+        self.assertEqual(literal, 345)
         
     def test_nextLiteral(self):
         cnf = [{-123:True, -312:True, 423:True, 123:True},
                {-123:True}]
         
         literal = nextLiteral(cnf)
-        self.assertEqual(literal, (-123, True))
+        self.assertEqual(literal, -123)
 
 if __name__ == '__main__':
     unittest.main()
