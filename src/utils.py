7ccce203e6c473c3a9f2059729f3a06ad3991bfe
Victor <victorzuanazzi@gmail.com>
2019-03-02 12:25:22 +0100

    function to generate syntetic data (just for fun)

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from abstract_heuristics import loadData
 import pandas as pd
 
 
@@ -54,4 +55,48 @@ def DIMACS_to_sudoku(assignment):
         matrix[v[0] - 1][v[1] - 1] = v[2]
     return matrix
 
+def fixData(df_x, df_y): #Not finished.
+    """not working"""
+    
+    x2y = []
+    unmatched_x = []
+    unmatched_y = df_y.index.tolist()
+    for x_idx, x_row in enumerate(df_x):
+        match = False
+        for y_idx in unmatched_y:
+            if ((x_row == df_y.loc[y_idx]) | (x_row == 0)).all():
+                match = True
+                break
+        if match:
+            unmatched_y.remove(y_idx)   
+        else:
+            x2y.append((x_idx, y_idx))
+            df_y.loc[x_idx] = df_y.loc[y_idx]
+            unmatched_x.append(x_idx)
+    return False #df_x, df_y
 
+def generateSynteticData(percentage = .6, path = '../data/SplitsLabel.csv', save = True):
+    """"Synteticly generate incomplete sudokus out of complete sudokus"""
+    
+    df_syntetic = pd.read_csv(path)
+    df_syntetic.reset_index(inplace = True, drop = True)
+    
+    nan_idx = df_syntetic[1].index[df_syntetic[1].apply(np.isnan)] 
+    if len(nan_idx) > 0:
+        df_syntetic.drop(labels = nan_idx, axis = 0, inplace = True)
+        df_syntetic.to_csv(path)
+    
+    for col in range(81):
+        rand_idx = np.random.randint(0, len(df_syntetic), int(len(df_syntetic)*percentage))
+        rand_idx.sort()
+        for r in rand_idx:
+            df_syntetic.loc[r][col] = 0
+            
+    if save:
+        name = "Syntetic"
+        path = path[-10:] + name + ".csv"
+        df_syntetic.to_csv(path)
+    
+    return df_syntetic
+    
+    
